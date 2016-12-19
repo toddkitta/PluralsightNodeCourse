559a5d5c8f986d3f720e6fced43683f7e2134fc1
Todd Kitta <tkitta@microsoft.com>
2016-12-19 01:56:38 -0600

    Updates

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
+var nav = [
+    {
+        Link: '/Books',
+        Text: 'Book'
+    },
+    {
+        Link: '/Authors',
+        Text: 'Author'
+    }
+];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+app.use('/Books', bookRouter);
+
 app.get('/', function (req, res) {
-    res.render('index', {title: 'Hello from render', list: ['a','b']});
+    res.render('index', {
+        title: 'Hello from render',
+        nav: nav
+    });
 });
 
 app.get('/books', function (req, res) {
